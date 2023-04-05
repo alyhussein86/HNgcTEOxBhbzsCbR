c01e0f9cafbaccbf3b9be787dd5546f985a3c1d5
alyhussein86 <111685594+alyhussein86@users.noreply.github.com>
2023-04-05 14:07:41 +0400

    #updated

--- multilinear-regression.py
+++ multilinear-regression.py
@@ -2,10 +2,6 @@ import pandas as pd
 from sklearn import linear_model
 
 reader = pd.read_csv("ACME-HappinessSurvey2020.csv")
-# mydict_list = list(reader)
-# for dicts in mydict_list:
-#     for keys in dicts:
-#         dicts[keys] = int(dicts[keys])
 
 y = reader['Y']
 X = reader[['X1', 'X2', 'X3', 'X4', 'X5', 'X6']]
@@ -15,4 +11,4 @@ regr.fit(X, y)
 
 predicted = regr.predict([[1, 1, 1, 1, 1, 1]])
 #  , 2, 1, 1, 5
-print(predicted)
+print(predicted*100)
